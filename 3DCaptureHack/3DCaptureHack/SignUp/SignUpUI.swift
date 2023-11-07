df9e141007cff25ac565e9004ae569b35f4826fb
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-11-07 11:32:42 +0100

    restructuredCode

--- 3DCaptureHack/3DCaptureHack/SignUp/SignUpUI.swift
+++ 3DCaptureHack/3DCaptureHack/SignUp/SignUpUI.swift
@@ -11,6 +11,8 @@ struct SignUpUI: View {
     @State var email = ""
     @State var password = ""
     @State var passwordAgain = ""
+    @State var username = ""
+    @State var showAlert = false
 
     var body: some View {
         if #available(iOS 15.0, *) {
@@ -36,17 +38,13 @@ struct SignUpUI: View {
                     Divider().padding()
 
                     VStack(spacing: 12) {
-                        TextField("Enter username", text: $email)
-                            .modifier(ConcaveGlassModifier())
+                        EnterUsernameView(username: $username)
 
-                        TextField("Enter email", text: $email)
-                            .modifier(ConcaveGlassModifier())
+                        EnterEmailView(email: $email)
 
-                        SecureField("Enter password", text: $password)
-                            .modifier(ConcaveGlassModifier())
+                        EnterPasswordView(password: $password)
 
-                        SecureField("Re-enter password", text: $passwordAgain)
-                            .modifier(ConcaveGlassModifier())
+                        ConfirmPasswordView(passwordAgain: $passwordAgain, password: $password, showAlert: showAlert)
                     }
                     .padding()
 
@@ -56,7 +54,9 @@ struct SignUpUI: View {
                         .font(.footnote)
 
                     Button {
-                        //TODO: - Faire l'action
+                        if password != passwordAgain {
+                            showAlert = true
+                        }
                     } label: {
                         ZStack {
                             Text("SIGN UP")
@@ -83,4 +83,81 @@ struct SignUpUI: View {
 
 #Preview {
     SignUpUI()
+}
+
+struct EnterUsernameView: View {
+    
+    @Binding var username: String
+    
+    var body: some View {
+        TextField("Enter username", text: $username)
+            .modifier(ConcaveGlassModifier())
+    }
+}
+
+
+
+struct EnterEmailView: View {
+    
+    @Binding var email: String
+    @State private var isEmailValid: Bool = true // État pour valider l'email
+    @State private var showAlert = false
+
+    var body: some View {
+        TextField("Enter email", text: $email)
+            .modifier(ConcaveGlassModifier())
+            .onChange(of: email, perform: { newValue in
+                // Valider l'email lorsque sa valeur change
+                isEmailValid = isEmailValid(email: newValue)
+            })
+            .overlay(
+                RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.red, lineWidth: 1)
+                    .opacity(isEmailValid ? 0 : 1)
+            )
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Format d'email invalide"),
+                    message: Text("Veuillez entrer un email valide au format nom@domaine.com."),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+            .onTapGesture {
+                if !isEmailValid {
+                    showAlert = true
+                }
+            }
+    }
+    
+    // Fonction de validation de l'email à l'aide d'une expression régulière
+    private func isEmailValid(email: String) -> Bool {
+        let emailRegex = #"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+}
+
+
+
+struct EnterPasswordView: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        SecureField("Enter password", text: $password)
+            .modifier(ConcaveGlassModifier())
+    }
+}
+
+struct ConfirmPasswordView: View {
+    
+    @Binding var passwordAgain: String
+    @Binding var password: String // Ajout de la liaison au mot de passe
+    @State  var showAlert: Bool
+
+    var body: some View {
+        SecureField("Re-enter password", text: $passwordAgain)
+            .modifier(ConcaveGlassModifier())
+            
+    }
 }
